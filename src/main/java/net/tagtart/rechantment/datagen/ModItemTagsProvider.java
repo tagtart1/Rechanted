edb798b55bbbcc23e5ced18c26b12471824ff36a
tagtart1 <100038117+tagtart1@users.noreply.github.com>
2026-02-17 14:22:37 -0600

    add weapon enchantable to rebirth tag

--- src/main/java/net/tagtart/rechantment/datagen/ModItemTagsProvider.java
+++ src/main/java/net/tagtart/rechantment/datagen/ModItemTagsProvider.java
@@ -16,39 +16,45 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModItemTagsProvider extends net.minecraft.data.tags.ItemTagsProvider {
 
-    public ModItemTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockLookup, ExistingFileHelper existingFileHelper) {
-        super(output, lookupProvider, blockLookup, Rechantment.MOD_ID, existingFileHelper);
-    }
+        public ModItemTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,
+                        CompletableFuture<TagLookup<Block>> blockLookup, ExistingFileHelper existingFileHelper) {
+                super(output, lookupProvider, blockLookup, Rechantment.MOD_ID, existingFileHelper);
+        }
 
-    public static final TagKey<Item> DIGGER_ITEM = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "digger_items"));
-    public static final TagKey<Item> OVERLOAD_ENCHANTABLE = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "overload_enchantable"));
-    public static final TagKey<Item> REACH_TOOLS = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "reach_tools"));
-    public static final TagKey<Item> REBIRTH_ENCHANTABLE = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "rebirth_enchantable"));
+        public static final TagKey<Item> DIGGER_ITEM = TagKey.create(Registries.ITEM,
+                        ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "digger_items"));
+        public static final TagKey<Item> OVERLOAD_ENCHANTABLE = TagKey.create(Registries.ITEM,
+                        ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "overload_enchantable"));
+        public static final TagKey<Item> REACH_TOOLS = TagKey.create(Registries.ITEM,
+                        ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "reach_tools"));
+        public static final TagKey<Item> REBIRTH_ENCHANTABLE = TagKey.create(Registries.ITEM,
+                        ResourceLocation.fromNamespaceAndPath(Rechantment.MOD_ID, "rebirth_enchantable"));
 
-    @Override
-    protected void addTags(HolderLookup.Provider provider) {
-        this.tag(DIGGER_ITEM)
-                .addTag(ItemTags.PICKAXES)
-                .addTag(ItemTags.AXES)
-                .addTag(ItemTags.SWORD_ENCHANTABLE)
-                .addTag(ItemTags.HOES);
+        @Override
+        protected void addTags(HolderLookup.Provider provider) {
+                this.tag(DIGGER_ITEM)
+                                .addTag(ItemTags.PICKAXES)
+                                .addTag(ItemTags.AXES)
+                                .addTag(ItemTags.SWORD_ENCHANTABLE)
+                                .addTag(ItemTags.HOES);
 
-        this.tag(OVERLOAD_ENCHANTABLE)
-                .addTag(ItemTags.CHEST_ARMOR_ENCHANTABLE)
-                .addOptional(ResourceLocation.withDefaultNamespace("elytra"));
+                this.tag(OVERLOAD_ENCHANTABLE)
+                                .addTag(ItemTags.CHEST_ARMOR_ENCHANTABLE)
+                                .addOptional(ResourceLocation.withDefaultNamespace("elytra"));
 
-        this.tag(REACH_TOOLS)
-                .addTag(ItemTags.PICKAXES)
-                .addTag(ItemTags.AXES)
-                .addTag(ItemTags.SHOVELS)
-                .addTag(ItemTags.HOES);
+                this.tag(REACH_TOOLS)
+                                .addTag(ItemTags.PICKAXES)
+                                .addTag(ItemTags.AXES)
+                                .addTag(ItemTags.SHOVELS)
+                                .addTag(ItemTags.HOES);
 
-        this.tag(REBIRTH_ENCHANTABLE)
-                .addTag(Tags.Items.TOOLS)
-                .addTag(ItemTags.HEAD_ARMOR_ENCHANTABLE)
-                .addTag(ItemTags.CHEST_ARMOR_ENCHANTABLE)
-                .addTag(ItemTags.LEG_ARMOR_ENCHANTABLE)
-                .addTag(ItemTags.FOOT_ARMOR_ENCHANTABLE)
-                .addOptional(ResourceLocation.withDefaultNamespace("elytra"));
-    }
+                this.tag(REBIRTH_ENCHANTABLE)
+                                .addTag(Tags.Items.TOOLS)
+                                .addTag(ItemTags.WEAPON_ENCHANTABLE)
+                                .addTag(ItemTags.HEAD_ARMOR_ENCHANTABLE)
+                                .addTag(ItemTags.CHEST_ARMOR_ENCHANTABLE)
+                                .addTag(ItemTags.LEG_ARMOR_ENCHANTABLE)
+                                .addTag(ItemTags.FOOT_ARMOR_ENCHANTABLE)
+                                .addOptional(ResourceLocation.withDefaultNamespace("elytra"));
+        }
 }
